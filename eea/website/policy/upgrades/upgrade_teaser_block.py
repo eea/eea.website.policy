e049db43a9f97c7c57650187e9cf3124035f340a
dobri1408 <dobriceanionut1408@gmail.com>
2024-04-08 13:45:21 +0300

    first working version

--- eea/website/policy/upgrades/upgrade_teaser_block.py
+++ eea/website/policy/upgrades/upgrade_teaser_block.py
@@ -1,40 +1,32 @@
-""" Upgrade to version 4.0
+""" Upgrade teaserBlock to gridBlock 
 """
-# pylint: disable=line-too-long
-import logging
+
 from Products.CMFCore.utils import getToolByName
-from eea.dexterity.indicators.interfaces import IIndicator
-
-logger = logging.getLogger("eea.dexterity.indicators")
 
 
-def convert_teaserGrid_to_gridblock(block_data):
-     grid_block = block_data.copy()
-     grid_block["@type"] = "gridBlock"
-     grid_block["blocks_layout"] = {
-          "items":list(map(lambda column:column["id"],block_data["columns"]))
-          }
-     grid_block["blocks"] ={x["id"] : x for x in block_data["columns"]}
-     del grid_block["columns"]
-     return grid_block
-                         
+def convert_teaser_grid_to_grid_block(block_data):
+    grid_block = block_data.copy()
+
+    grid_block["@type"] = "gridBlock"
+    grid_block["blocks_layout"] = {
+        "items": list(map(lambda column: column["id"], block_data["columns"]))
+    }
+    grid_block["blocks"] = {x["id"]: x for x in block_data["columns"]}
+    del grid_block["columns"]
+    return grid_block
+
+
 def upgrade_teaser_block(context):
-    """ Fix Indicator schema """
+    """Fix Indicator schema"""
     ctool = getToolByName(context, "portal_catalog")
-    #create a list of all content from the site
-    docsBrain = list(ctool.getAllBrains())
-    for brain in docsBrain:
-        #retrive block data from the item 
+    # create a list of all content from the site
+    brains_docs = list(ctool.getAllBrains())
+    for brain in brains_docs:
+        # retrive block data from the item
         doc = brain.getObject()
-        blocks = getattr(doc, "blocks", None)
+        blocks = getattr(doc, "blocks", [])
         for key in blocks.keys():
-                if blocks[key]["@type"] == "teaserGrid":
-                   blocks[key] = convert_teaserGrid_to_gridblock(blocks[key])
-                   print(blocks[key])
-        doc.blocks = blocks;
-
-                     
-                     
-                       
-   
-
+            # we have found a teaserGrid
+            if blocks[key]["@type"] == "teaserGrid":
+                blocks[key] = convert_teaser_grid_to_grid_block(blocks[key])
+        doc.blocks = blocks
