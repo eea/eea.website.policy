6ba3ec0d2d38a01f97de1a1b379e2a68c1445aed
dobri1408 <50819975+dobri1408@users.noreply.github.com>
2024-04-12 08:59:52 +0300

    Update upgrade step to respect the right way of making an upgrade step

--- eea/website/policy/upgrades/upgrade_teaser_block.py
+++ eea/website/policy/upgrades/upgrade_teaser_block.py
@@ -1,6 +1,16 @@
 """ Upgrade teaserBlock to gridBlock """
 
-from Products.CMFCore.utils import getToolByName
+from plone.protect.interfaces import IDisableCSRFProtection
+from plone.restapi.blocks import visit_blocks
+from Products.Five.browser import BrowserView
+from zope.interface import alsoProvides
+from zope.lifecycleevent import modified
+
+import logging
+import transaction
+
+logger = logging.getLogger("convert_teaser_to_grid_block")
+logger.setLevel(logging.INFO)
 
 
 def convert_teaser_to_grid_block(block_data):
@@ -16,18 +26,33 @@ def convert_teaser_to_grid_block(block_data):
     return grid_block
 
 
-def upgrade_teaser_block(context):
+def upgrade_teaser_block(portal):
     """Upgrade all teaserGrid blocks to gridBlock"""
-    ctool = getToolByName(context, "portal_catalog")
-    # create a list of all content from the site
-    brains_docs = list(ctool.getAllBrains())
-    for brain in brains_docs:
+    i = 0
+    output = ""
+    for brain in portal.portal_catalog(
+        object_provides="plone.restapi.behaviors.IBlocks"
+    ):
+        obj = brain.getObject()
+        blocks = obj.blocks
+        logger.info(f"Processing {obj.absolute_url()}")
+        output += f"Processing {obj.absolute_url()}\n"
         # retrive block data from the item
-        doc = brain.getObject()
-        blocks = getattr(doc, "blocks", {})
-        if blocks.keys():
-            for key in blocks.keys():
-                # we have found a teaserGrid
-                if blocks[key]["@type"] == "teaserGrid":
-                    blocks[key] = convert_teaser_to_grid_block(blocks[key])
-            doc.blocks = blocks
+        for block in visit_blocks(obj, blocks):
+            # we have found a teaserGrid
+            if block.get("@type", False) and block["@type"] == "teaserGrid":
+                new_block = block.copy()
+                new_block = convert_teaser_to_grid_block(block)
+                block.clear()
+                block.update(new_block)
+                logger.info(f"{obj.absolute_url()} - Updated")
+                output += f"{obj.absolute_url()} - Updated \n"
+
+        obj.blocks = blocks
+        modified(obj)
+        i += 1
+        if not i % 100:
+            logger.info(i)
+            transaction.commit()
+    transaction.commit()
+    return output
