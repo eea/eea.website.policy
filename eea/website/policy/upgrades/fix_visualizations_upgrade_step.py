fc352361102f65f064d07a6935b955e4166ecb97
dobri1408 <dobriceanionut1408@gmail.com>
2025-01-31 20:51:14 +0200

    fix provider url

--- eea/website/policy/upgrades/fix_visualizations_upgrade_step.py
+++ eea/website/policy/upgrades/fix_visualizations_upgrade_step.py
@@ -1,4 +1,5 @@
 """Upgrade step to transform provider_url to resolve uid if the case"""
+
 from urllib.parse import urlparse
 import transaction
 from zope.lifecycleevent import modified
@@ -23,15 +24,19 @@ def upgrade_visualizations(portal):
         except Exception:
             continue  # Skip objects that cannot be retrieved
 
-        viz = getattr(obj, 'visualization', None)
+        viz = getattr(obj, "visualization", None)
 
         if not viz or not isinstance(viz, dict):
             continue
 
-        provider_url = viz.get('provider_url', None)
+        provider_url = viz.get("provider_url", None)
 
-        if provider_url and 'resolveuid' in provider_url and  viz['provider_url'].startswith("../"):
-            viz['provider_url'] =  viz['provider_url'][3:];
+        if (
+            provider_url
+            and "resolveuid" in provider_url
+            and viz["provider_url"].startswith("../")
+        ):
+            viz["provider_url"] = viz["provider_url"][3:]
             obj.visualization = viz
             modified(obj)
             i += 1
